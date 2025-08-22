997a5887823e3a100f3d51ef1e051e8551e3c3b7
Jon West <electrikjesus@gmail.com>
2025-08-22 13:06:41 -0400

    Also remove ql-ril.conf from standard build

--- aosptree/vendor/devices-community/gd_rpi4/gd_rpi4.mk
+++ aosptree/vendor/devices-community/gd_rpi4/gd_rpi4.mk
@@ -46,8 +46,7 @@ PRODUCT_PACKAGES += cromite-webview
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    $(LOCAL_PATH)/cellular/vendor/etc/ql-ril.conf:$(TARGET_COPY_OUT_VENDOR)/etc/ql-ril.conf
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml 
 
 
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
