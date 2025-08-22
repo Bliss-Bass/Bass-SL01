101d311d9d42bfd3c302eb7b5c16660cf148322d
Jon West <electrikjesus@gmail.com>
2025-08-22 12:53:47 -0400

    remove modem properties from here

--- aosptree/vendor/devices-community/gd_rpi4/gd_rpi4.mk
+++ aosptree/vendor/devices-community/gd_rpi4/gd_rpi4.mk
@@ -44,32 +44,6 @@ GD_BUILD_QUECTEL_GPS := false
 
 PRODUCT_PACKAGES += cromite-webview
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    hw.nophone=no \
-    ro.boot.noril=no \
-    ro.radio.noril=no \
-    ro.telephony.default_network=10 \
-    keyguard.no_require_sim=true \
-    ril.function.dataonly=0 \
-    ril.subscription.types=SIM \
-    persist.telephony.support.ipv6=1 \
-    persist.telephony.support.ipv4=1 \
-    telephony.lteOnGsmDevice=1 \
-    telephony.lteOnCdmaDevice=1 \
-    ro.telephony.call_ring.delay=0 \
-    ro.ril.enable.amr.wideband=1 \
-    ring.delay=0 \
-    ro.config.vc_call_steps=20 \
-    persist.cust.tel.eons=1 \
-    ro.config.hw_fast_dormancy=1 \
-    ro.radio.networkmode=enable \
-    persist.audio.fluence.voicecall=false \
-    ro.com.android.dataroaming=true \
-    ro.telephony.get_imsi_from_sim=true \
-    ro.telephony.ril.config=simactivation \
-    ro.factory.hasGPS=true
-
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
